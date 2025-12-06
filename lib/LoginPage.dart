34abdfba1622e1be86c307e92f638ebbe50d5a0e
Leonis <franciscoleonelfds@gmail.com>
2025-12-06 11:51:43 -0300

    Adição SafeArea

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -9,175 +9,188 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text("Minha Tela Nova")),
       backgroundColor: Color.fromARGB(255, 0, 31, 63),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 10),
-          Image.asset("assets/images/falcon.png", scale: 1.5,),
+      resizeToAvoidBottomInset: true, // 🔥 IMPORTANTE
 
-          Text(
-            "Sistema de Ocorrências",
-            style: GoogleFonts.poppins(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              shadows: [
-                Shadow(
-                  offset: const Offset(0, 1),
-                  blurRadius: 3.0,
-                  color: Colors.black.withValues(alpha: 0.3),
-                ),
-              ],
-            ),
-          ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
 
-          Container(
-            width: 366,
-            height: 50,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              Image.asset("assets/images/falcon.png", scale: 3),
 
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 38, 64, 92),
-              borderRadius: BorderRadius.circular(20.0), // Cantos arredondados
-            ),
-
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-
-            child: Center(
-              child: Text(
-                "Acesso exclusivo para agentes autorizados do PMMA",
-                textAlign: TextAlign.center,
+              Text(
+                "Sistema de Ocorrências",
                 style: GoogleFonts.poppins(
-                  color: Colors.white.withValues(alpha: 0.9),
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  shadows: [
+                    Shadow(
+                      offset: const Offset(0, 1),
+                      blurRadius: 3.0,
+                      color: Colors.black.withOpacity(0.3),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ),
+              Container(
+                width: 366,
+                height: 50,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 10,
+                ),
 
-          const SizedBox(height: 45),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 38, 64, 92),
+                  borderRadius: BorderRadius.circular(
+                    20.0,
+                  ), // Cantos arredondados
+                ),
 
-          Container(
-            width: 366,
-            // Para preencher a largura
-            height: 380,
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0), // Cantos arredondados
-            ),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
 
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                children: [
-                  const SizedBox(height: 10),
+                child: Center(
+                  child: Text(
+                    "Acesso exclusivo para agentes autorizados do PMMA",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white.withValues(alpha: 0.9),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              const SizedBox(height: 20),
+
+              Container(
+                width: 346,
+                // Para preencher a largura
+                height: 380,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    20.0,
+                  ), // Cantos arredondados
+                ),
+
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
                     children: [
-                      Icon(
-                        Icons.shield,
-                        color: Color.fromARGB(255, 38, 64, 92),
-                        size: 28,
+                      const SizedBox(height: 10),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.shield,
+                            color: Color.fromARGB(255, 38, 64, 92),
+                            size: 28,
+                          ),
+
+                          const SizedBox(width: 10),
+                          Text(
+                            'Login do Agente',
+                            style: GoogleFonts.poppins(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 38, 64, 92),
+                            ),
+                          ),
+                        ],
                       ),
 
-                      const SizedBox(width: 10),
-                      Text(
-                        'Login do Agente',
-                        style: GoogleFonts.poppins(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+                      const SizedBox(height: 10),
+
+                      Container(
+                        height: 4,
+                        width: 60,
+                        decoration: BoxDecoration(
                           color: Color.fromARGB(255, 38, 64, 92),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+
+                      const SizedBox(height: 24),
+
+                      AppTextInput(
+                        hintText: "Email",
+                        prefixIcon: const Icon(Icons.mail_outline),
+                      ),
+
+                      const SizedBox(height: 24),
+
+                      AppTextInput(
+                        hintText: "Senha",
+                        obscureText: true, // ativa o botão de mostrar/ocultar
+                        prefixIcon: const Icon(Icons.lock_outline),
+                      ),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Esqueceu a senha?',
+                            style: GoogleFonts.poppins(
+                              color: Color.fromARGB(255, 38, 64, 92),
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      ReusableButton(
+                        text: "Entrar",
+                        width: double.infinity,
+                        onPressed: () {},
+                        color: Color.fromARGB(255, 0, 51, 153),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 12, top: 8),
+                        child: Center(
+                          child: Text(
+                            'Versão 1.0.3',
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 12,
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-
-                  const SizedBox(height: 10),
-
-                  Container(
-                    height: 4,
-                    width: 60,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 38, 64, 92),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-
-                  const SizedBox(height: 24),
-
-                  AppTextInput(
-                    hintText: "Email",
-                    prefixIcon: const Icon(Icons.mail_outline),
-                  ),
-
-                  const SizedBox(height: 24),
-
-                  AppTextInput(
-                    hintText: "Senha",
-                    obscureText: true, // ativa o botão de mostrar/ocultar
-                    prefixIcon: const Icon(Icons.lock_outline),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerRight,
-
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Esqueceu a senha?',
-                        style: GoogleFonts.poppins(
-                          color: Color.fromARGB(255, 38, 64, 92),
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  ReusableButton(
-                    text: "Entrar",
-                    width: double.infinity,
-                    onPressed: () {},
-                    color: Color.fromARGB(255, 0, 51, 153),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 12, top: 8),
-                    child: Center(
-                      child: Text(
-                        'Versão 1.0.3',
-                        style: GoogleFonts.poppins(
-                          color: Colors.black,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                ],
-
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(bottom: 0, top: 15),
-            child: Center(
-              child: Text(
-                'Desenvolvido para a Polícia Militar do Maranhão',
-                style: GoogleFonts.poppins(
-                  color: Colors.white70,
-                  fontSize: 12,
                 ),
               ),
-            ),
-          ),
 
-        ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 0, top: 15),
+                child: Center(
+                  child: Text(
+                    'Desenvolvido para a Polícia Militar do Maranhão',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white70,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
